7805e72665728f0e29001ff7536aebf4abfa4f53
Krabbyz <austinsworkstuffs@gmail.com>
2023-06-11 20:10:38 -0700

    read and write data to json

--- back-end/inventory/tracking/models.py
+++ back-end/inventory/tracking/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class GCItem(models.Model):
+    key = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+    amount_per_box = models.IntegerField()
+    amount = models.IntegerField()
+    sku = models.CharField(max_length=200)
+    image_url = models.CharField(max_length=200)
--- back-end/inventory/tracking/views.py
+++ back-end/inventory/tracking/views.py
@@ -1,5 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from rest_framework.decorators import action
+import json
 
 class FooViewSet(ViewSet):
     # TODO(austin): remove me -- make this real
@@ -8,3 +10,20 @@ class FooViewSet(ViewSet):
             "hello": "Austin",
             "hi": "Long"
         })
+    
+    @action(detail=False, methods=['get'])
+    def getData(self, request):
+        with open('/back-end/inventory/tracking/data/data.json', 'r') as file:
+            data = file.read()
+        jsonData = json.loads(data)
+
+        return Response(jsonData)
+    
+    @action(detail=False, methods=['post'])
+    def postData(self, request, *args, **kwargs):
+        data = request.data
+        with open('/back-end/inventory/tracking/data/data.json', 'w') as file:  # open in write mode
+            json.dump(data, file)  # write the data to the json file
+        return Response({"message": "Success!"})
+    
+
